918512672eb1635fe94642b57641faf020adeace
Riccardo <rickydeluca1@gmail.com>
2023-03-10 15:34:09 +0100

    save test metrics directy with main

--- log_utils.py
+++ log_utils.py
@@ -1,6 +1,116 @@
 import os
 import sys
+import csv
 from pytorch_lightning.loggers import CSVLogger
+
+def count_params(model):
+    """
+    Return the number of trainable parameters of the model.
+    """
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def save_results(model, test_metrics, hyperparams):
+    """
+    Store the test metrics in a csv file
+    """
+
+    test = hyperparams['test']
+    outfile = hyperparams['res_dir']
+    total_params = count_params(model)
+
+    # ================================
+    #     SUBSPACE_DIM VS TEST_ACC    
+    # ================================
+    if test == "subspace":
+        # Define the outfile path wrt the hyperparams
+        outfile += f"/subspace_{hyperparams['dataset']}"
+
+        if hyperparams['shuffle_pixels']:
+            outfile += f"_shuffled_pixels"
+        
+        if hyperparams['shuffle_labels']:
+            outfile += f"_shuffled_labels"
+        
+        outfile += f"_{hyperparams['network_type']}"
+
+        if hyperparams['network_type'] == 'fc':
+            outfile += f"_depth_{hyperparams['hidden_depth']}_width_{hyperparams['hidden_width']}"
+        else:
+            outfile += f"_nfeatures_{hyperparams['n_feature']}"
+        
+        outfile += f"_epochs_{hyperparams['epochs']}_lr_{hyperparams['lr']}.csv"
+
+        # Check if the outfile exist and if it is empty
+        if os.path.isfile(outfile) and os.path.getsize(outfile) > 0:
+            # Append data
+            with open(outfile, mode='a', newline='') as of:
+                writer = csv.writer(of)
+                row = [hyperparams['subspace_dim'], test_metrics['test_acc'], test_metrics['test_loss']]
+                writer.writerow(row)
+
+        else:
+            # Write data
+            header = ["subspace_dim", "test_acc", "test_loss"]
+            
+            with open(outfile, mode='w', newline='') as of:
+                writer = csv.writer(of)
+
+                # Write the header
+                writer.writerow(header)
+
+                # Write data
+                row = [hyperparams['subspace_dim'], test_metrics['test_acc'], test_metrics['test_loss']]
+                writer.writerow(row)
+
+    # ============================
+    #     N_PARAMS VS BASELINE    
+    # ============================
+    if test == "baseline":
+        # Define the outfile path name wrt the hyperparams
+        outfile += f"/baseline_{hyperparams['dataset']}"
+        
+        if hyperparams['shuffle_pixels']:
+            outfile += f"_shuffled_pixels"
+        
+        if hyperparams['shuffle_labels']:
+            outfile += f"_shuffled_labels"
+
+        outfile += f"_{hyperparams['network_type']}_epochs_{hyperparams['epochs']}_lr_{hyperparams['lr']}.csv"
+
+        # Check if the outfile exist and it is empty
+        if os.path.isfile(outfile) and os.path.getsize(outfile) > 0:
+            # Append data
+            with open(outfile, mode='a', newline='') as of:
+                writer = csv.writer(of)
+
+                if hyperparams['network_type'] == 'fc':
+                    row = [hyperparams['hidden_depth'], hyperparams['hidden_width'], total_params, test_metrics['test_acc']]
+                else:
+                    row = [hyperparams['n_feature'], total_params, test_metrics['test_acc']]
+
+                writer.writerow(row)
+
+        else:
+            # Create file
+            if hyperparams['network_type'] == 'fc': 
+                header = ["depth", "width", "total_params", "baseline"]
+            else:
+                header = ["n_feature", "total_params", "baseline"]
+            
+            with open(outfile, mode='w', newline='') as of:
+                writer = csv.writer(of)
+
+                # Write the header
+                writer.writerow(header)
+
+                # Write data
+                if hyperparams['network_type'] == 'fc':
+                    row = [hyperparams['hidden_depth'], hyperparams['hidden_width'], total_params, test_metrics['test_acc']]
+                else:
+                    row = [hyperparams['n_feature'], total_params, test_metrics['test_acc']]
+
+                writer.writerow(row)
+
 
 class CustomCSVLogger(CSVLogger):
     """
